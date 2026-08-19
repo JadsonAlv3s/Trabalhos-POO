60766915f199b8fc1eea5add1832bf9b1eea46fe
Jadson Alves <jadson.almeida@escolar.ifrn.edu.br>
2026-08-19 11:09:31 -0300

    Aula Intruducao

---
+++ Int.py
@@ -0,0 +1,61 @@
+nome = "Jadson"
+numero = 1
+numero_float = 1.2
+numero_complexo = 3+4j
+booleano_v = True
+booleano_f = False
+
+#operadores aritmeticos: +, -, /, *, **, //, %
+
+#operadores relacionais: >, <, >=, <=, !=, ==
+
+#operadores logicos: and, or, not 
+
+# Entrada e saida de dados padrao
+
+nome = input("Digite seu nome: ") #O retorno do input() sempre sera uma string
+
+numero = int(input("Digite um numero: "))
+
+print (nome)
+print (f"seu nome e {nome}")
+sobrenome = input("Digite seu sobrenome")
+print(f"nome completo: {nome_completo}")
+
+
+#########Estruturas COndicionais#############
+
+#if
+
+if (numero > 3):
+    print("O valor é maior que 3")
+
+if (nome == "jadson"):
+    print(f"Seu nome é {nome}")
+else:
+    print(f"Seu nome nao é {nome}")
+
+            
+if (nome == "jadson"):
+    print(f"Seu nome é {nome}")
+
+elife (numero == ""):
+    print(f"Seu nome é {nome}")
+else:
+    print(f"Seu nome nao é {nome}")
+
+#############Estrutura de Repetição##########
+
+
+for i in range(5):
+    print(i)
+
+for i in range(3, 7):
+    print(i)
+
+        
+for in range (3, 7, 2):
+    printi(i)
+
+    
+
